f5630c15ddb353e59da4cec52876f6d0516a574e
Christoph Schöni <christoph.schoeni@gmail.com>
2022-02-05 07:58:34 +0100

    Udpate Django Project

---
+++ projectDjango/mySchool/students/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    birthdate = models.DateField(null=False, blank=False)
